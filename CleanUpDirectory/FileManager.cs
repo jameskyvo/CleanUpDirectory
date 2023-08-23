027337617afcddbbf544801bf2459eef9664baa4
James Kelley <james.kelley.netdev@gmail.com>
2023-08-23 09:22:07 -0500

    Add project files.

---
+++ CleanUpDirectory/FileManager.cs
@@ -0,0 +1,24 @@
+﻿public class FileManager
+{
+    public static void RemoveInactiveFiles(string dirToClean, int daysBeforeClean, SearchOption searchOption)
+    {
+        string allFileExtensions = "*.*";
+        DirectoryInfo directoryInfo = new DirectoryInfo(dirToClean);
+        List<FileInfo> files = directoryInfo.GetFiles(allFileExtensions, searchOption).ToList();
+
+        foreach (FileInfo file in files)
+        {
+            if (DateTime.Now - file.LastWriteTime >= TimeSpan.FromDays(daysBeforeClean))
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"There was an error deleting {file.Name} Exception: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ CleanUpDirectory/Program.cs
@@ -0,0 +1,19 @@
+﻿using static FileManager;
+
+if (args.Length == 3)
+{
+    // Command Line Arguments
+    string dirToClean = args[0];
+    int daysBeforeClean = int.Parse(args[1]);
+    bool isRecursive = bool.Parse(args[2].ToLower());
+    SearchOption searchOption;
+
+    // Sets the search option according to the value of isRecursive
+    searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+    FileManager.RemoveInactiveFiles(dirToClean, daysBeforeClean, searchOption);
+}
+else
+{
+    throw new Exception("Invalid number of arguments provided. Usage: Program <dirToClean> <daysBeforeClean> <isRecursive>");
+}
